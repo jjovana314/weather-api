f91b1e3ac1a5f68db5b3dec22ce753974c04058f
Jovana <jjovana314@gmail.com>
2020-08-20 19:18:41 +0200

    schema modified

--- web/schema.py
+++ web/schema.py
@@ -1,53 +1,51 @@
-"""
-This is schema for Weather API.
-
-* Register
-    - POST
-        /register
-    - username, password
-    - OK 200
-    - INVALID_USERNAME 301
-    - KEYS_NOT_VALID 306
-    * Return if OK:
-        {
-            "Message": "You successfully signed for an API.",
-            "Code": 200
+register = {
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string", "minLength": 3, "maxLength": 20
+        },
+        "password": {
+            "type": "string", "minLength": 7, "maxLength": 20
         }
+    }
+}
 
-* Weather
-    - POST
-        /weather
-    - username, password, country, city, format_temperature (validation)
-    - OK 200
-    - INVALID_USERNAME 301
-    - INVALID_PASSWORD 302
-    - OUT_OF_TOKENS 303
-    - DATA_NOT_EXIST 304
-    - KEYS_NOT_vALID 306
-    * Return if OK:
-        {
-            "Message": "This is weather for *CITY*, *COUNTRY*: {
-                "Temperature": *FORMAT_TEMPERATURE*,
-                "Minimum temperature": *FORMAT_TEMPERATURE*,
-                "Maximum temperature": *FORMAT_TEMPERATURE*,
-                "Feels like": *FORMAT_TEMPERATURE*,
-                "Description": *str*,
-                "Wind speed": *float*
-            },
-            "Code": 200
+weather = {
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string", "minLength": 3, "maxLength": 20
+        },
+        "password": {
+            "type": "string", "minLength": 7, "maxLength": 20
+        },
+        "country": {
+            "type": "string", "minLength": 2, "maxLength": 50
+        },
+        "city": {
+            "type": "string", "minLength": 2, "maxLength": 50
+        },
+        "format_temperature": {
+            "type": "string", "minLength": 1, "maxLength": 1
         }
+    }
+}
 
-* Refill
-    - POST
-        /refill
-    - username, admin_pwd, amout
-    - OK 200
-    - INVALID_USERNAME 301
-    - INVALID_ADMIN_PASSWORD 305
-    - KEYS_NOT_VALID 306
-    * Return if OK:
-        {
-            "Message": "Tokens added successfully.",
-            "Code": 200
+refill = {
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string", "minLength": 3, "maxLength": 20
+        },
+        "password": {
+            "type": "string", "minLength": 7, "maxLength": 20
+        },
+        "admin_pwd": {
+            "type": "string", "minLength": 9, "maxLength": 9
+        },
+        "amount": {
+            "type": "integer", "minimum": 1, "maximum": 20
         }
-"""
+    }
+}
+
